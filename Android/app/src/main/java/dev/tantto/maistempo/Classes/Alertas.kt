d34a42cf6e07a27e4033d4ad4e8c6455ac563107
Gabriel Luan <gabriel.luan@tantto.com.br>
2019-07-17 17:10:35 -0300

    Tela de login finalizada com toda a autenticacao e envio de dados

---
+++ Android/app/src/main/java/dev/tantto/maistempo/Classes/Alertas.kt
@@ -0,0 +1,22 @@
+package dev.tantto.maistempo.Classes
+
+import android.app.Activity
+import com.tapadoo.alerter.Alerter
+import dev.tantto.maistempo.R
+
+class Alertas {
+
+    companion object {
+
+        fun CriarTela(Tela: Activity, Mensagem:Int, Titulo:Int, Duracao:Long = 10000) : Alerter{
+            val Alerta = Alerter.create(Tela)
+            Alerta.setText(Mensagem)
+            Alerta.setTitle(Titulo)
+            Alerta.setBackgroundColor(R.color.colorPrimary)
+            Alerta.setDuration(Duracao)
+            return  Alerta
+        }
+
+    }
+
+}
--- Android/app/src/main/java/dev/tantto/maistempo/Fragmentos/FragmentLogin.kt
+++ Android/app/src/main/java/dev/tantto/maistempo/Fragmentos/FragmentLogin.kt
@@ -2,9 +2,7 @@ package dev.tantto.maistempo.Fragmentos
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,27 +10,23 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.SignInButton
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import dev.tantto.maistempo.Interfaces.EnumTelas
+import com.google.firebase.auth.FirebaseUser
+import dev.tantto.maistempo.Classes.Alertas
+import dev.tantto.maistempo.Google.AutenticacaoLogin
+import dev.tantto.maistempo.Google.GoogleFirebaseAutenticacao
+import dev.tantto.maistempo.Google.TiposErrosLogar
 import dev.tantto.maistempo.R
 import dev.tantto.maistempo.Telas.TelaLogin
 
 @SuppressLint("ValidFragment")
-class FragmentLogin(private var Contexto:Context, private var ReferecenciaTela:TelaLogin) : Fragment(){
+class FragmentLogin(private var Contexto:Context, private var ReferecenciaTela:TelaLogin) : Fragment(), AutenticacaoLogin{
 
     var UserName:EditText? = null
     var Senha:EditText? = null
     var SalvarUsuario:CheckBox? = null
     var Conectar:Button? = null
-    var ConectarGoole:SignInButton? = null
+    private var BotaoApresentacao:Button? = null
+    private var BotaoNovo:Button? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val View = LayoutInflater.from(Contexto).inflate(R.layout.fragment_login, container, false)
@@ -40,26 +34,46 @@ class FragmentLogin(private var Contexto:Context, private var ReferecenciaTela:T
         return View
     }
 
-
     private fun RecuperarView(View: View) {
         UserName = View.findViewById(R.id.UserName)
         Senha = View.findViewById(R.id.Senha)
         SalvarUsuario = View.findViewById(R.id.LembrarLogin)
         Conectar = View.findViewById(R.id.Conectar)
-        ConectarGoole = View.findViewById(R.id.ConectarGoogle)
+        BotaoApresentacao = View.findViewById<Button>(R.id.IrApresentacao)
+        BotaoNovo = View.findViewById<Button>(R.id.IrNovo)
 
+        Eventos()
+
+    }
+
+    private fun Eventos() {
         Conectar?.setOnClickListener {
             Verificar()
         }
 
-        ConectarGoole?.setOnClickListener {
+        BotaoApresentacao?.setOnClickListener {
+            ReferecenciaTela.MudarTela(0)
         }
-
+        BotaoNovo?.setOnClickListener {
+            ReferecenciaTela.MudarTela(2)
+        }
     }
 
     private fun Verificar(){
         if(UserName?.text?.isNotEmpty()!! && Senha?.text?.isNotEmpty()!!){
+            GoogleFirebaseAutenticacao.LogarUsuario(UserName?.text.toString(), Senha?.text.toString(), this)
+        }
+    }
 
+    override fun UsuarioLogado(User: FirebaseUser) {
+        ReferecenciaTela.LoginConcluido(User)
+    }
+
+    override fun ErroLogar(Erro: TiposErrosLogar) {
+        if(Erro == TiposErrosLogar.SENHA_INCORRETA){
+            Alertas.CriarTela(ReferecenciaTela, R.string.SenhaIncorreta, R.string.Atencao, 5000).show()
+        } else if(Erro == TiposErrosLogar.CONTA_NAO_EXISTENTE){
+            Alertas.CriarTela(ReferecenciaTela, R.string.ContaNaoExistente, R.string.Atencao, 5000).show()
         }
     }
 }
