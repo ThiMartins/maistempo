862b8ce37c96d94492999b0074f33daf8792b7b1
Gabriel Luan <gabriel.luan@tantto.com.br>
2019-08-27 16:48:41 -0300

    Melhorias no design e finalização da implementação da barra de progresso

--- Android/app/src/main/java/dev/tantto/maistempo/Fragmentos/FragmentResumo.kt
+++ Android/app/src/main/java/dev/tantto/maistempo/Fragmentos/FragmentResumo.kt
@@ -1,15 +1,19 @@
 package dev.tantto.maistempo.fragmentos
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import dev.tantto.barra_progresso.BarraProgresso
 import dev.tantto.barra_progresso.OnBarraChanged
@@ -19,6 +23,7 @@ import dev.tantto.maistempo.classes.Alertas
 import dev.tantto.maistempo.google.*
 import dev.tantto.maistempo.modelos.Lojas
 import dev.tantto.maistempo.R
+import dev.tantto.maistempo.classes.Dados
 import dev.tantto.maistempo.telas.TelaResumoLoja
 import java.util.*
 
@@ -44,7 +49,6 @@ class FragmentResumo : Fragment(), FunctionsInterface {
         super.onResume()
         configurandoView()
         configurandoAdapter()
-
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -87,47 +91,78 @@ class FragmentResumo : Fragment(), FunctionsInterface {
             }
 
             override fun setOnFinishListener(v: BarraProgresso?) {
-                val email = FirebaseAutenticacao.Autenticacao.currentUser?.email
-                if (!email.isNullOrEmpty()) {
-                    mostrarAlerta()
-                    DatabaseFirebaseSalvar.adicionarPontos(email, 1, TipoPontos.PONTOS_FILA)
-
-                    val Horas = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
-                    val Tipo = when {
-                        Tabs?.selectedTabPosition == 0 -> TipoFila.FilaNormal
-                        Tabs?.selectedTabPosition == 1 -> TipoFila.FilaRapida
-                        Tabs?.selectedTabPosition == 2 -> TipoFila.FilaPrererencial
-                        else -> null
+                if(ProgressoFila?.isEnabled!!){
+                    val ConfirmarcaoBuilder = AlertDialog.Builder(context)
+                    ConfirmarcaoBuilder.setTitle(R.string.Atencao)
+                    ConfirmarcaoBuilder.setMessage(R.string.DesejaEnvia)
+                    ConfirmarcaoBuilder.setPositiveButton(R.string.Sim){ _, _ ->
+                        enviarNota()
                     }
-
-
-                    val valor = ProgressoFila?.progresso!!
-                    val valorNota = when {
-                        valor < 14.28 -> 0
-                        valor >= 14.28 && valor < 28.56 -> 1
-                        valor >= 28.56 && valor < 42.84 -> 2
-                        valor >= 42.84 && valor < 57.12 -> 3
-                        valor >= 57.12 && valor < 71.4 -> 4
-                        valor >= 71.4 && valor < 85.68 -> 5
-                        else -> 6
-                    }
-
-                    CloudFunctions.adicionarNotaFila(LojaInfo?.id!!, Tipo?.valor!!, valorNota.toDouble(), Horas.toString(), this@FragmentResumo)
-                    if(referencia != null){
-                        Alertas.criarAlerter(referencia!!, R.string.EnviandoNota ,R.string.Enviando, 10000).show()
-                    }
+                    ConfirmarcaoBuilder.setNegativeButton(R.string.Nao, null)
+                    ConfirmarcaoBuilder.create().show()
+                } else {
+                    Snackbar.make(this@FragmentResumo.view?.findViewById<NestedScrollView>(R.id.FragmentoFilaId)!!, R.string.ErroEnviarNovamente, Snackbar.LENGTH_LONG).show()
                 }
             }
         })
 
+        val contexto = context
+        if(contexto != null){
+            val Horas = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+            val Dia = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+            val Momento = Dados.verificarLojaHorario(contexto, LojaInfo?.id!!)
+            if(Momento == "$Horas:$Dia"){
+                ProgressoFila?.isEnabled = false
+            }
+        }
+
         NumeroAvaliacoes?.text = String.format(LojaInfo?.quantidadeAvaliacoesFila.toString() + " "+ getString(R.string.Avalicoes))
+    }
+
+    private fun enviarNota() {
+        val email = FirebaseAutenticacao.Autenticacao.currentUser?.email
+        if (!email.isNullOrEmpty()) {
+            mostrarAlerta()
+            DatabaseFirebaseSalvar.adicionarPontos(email, 1, TipoPontos.PONTOS_FILA)
+
+            val Horas = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+            val Tipo = when {
+                Tabs?.selectedTabPosition == 0 -> TipoFila.FilaNormal
+                Tabs?.selectedTabPosition == 1 -> TipoFila.FilaRapida
+                Tabs?.selectedTabPosition == 2 -> TipoFila.FilaPrererencial
+                else -> null
+            }
+
+
+            val valor = ProgressoFila?.progresso!!
+            val valorNota = when {
+                valor < 14.28 -> 0
+                valor >= 14.28 && valor < 28.56 -> 1
+                valor >= 28.56 && valor < 42.84 -> 2
+                valor >= 42.84 && valor < 57.12 -> 3
+                valor >= 57.12 && valor < 71.4 -> 4
+                valor >= 71.4 && valor < 85.68 -> 5
+                else -> 6
+            }
+
+            CloudFunctions.adicionarNotaFila(
+                LojaInfo?.id!!,
+                Tipo?.valor!!,
+                valorNota.toDouble(),
+                Horas.toString(),
+                this@FragmentResumo
+            )
+            if (referencia != null) {
+                Alertas.criarAlerter(referencia!!, R.string.EnviandoNota, R.string.Enviando, 10000)
+                    .show()
+            }
+        }
     }
 
     private fun recuperandoView() {
         Lista = this.view?.findViewById(R.id.ListaFila)
         NumeroAvaliacoes = this.view?.findViewById(R.id.NumeroAvaliacoes)
         Tabs = this.view?.findViewById(R.id.TabLayoutFila)
-        //EnviarFilaMomento = this.view?.findViewById(R.id.EnviarFila)
         ProgressoFila = this.view?.findViewById(R.id.ProgressoFilaVoto)
     }
 
@@ -152,10 +187,18 @@ class FragmentResumo : Fragment(), FunctionsInterface {
     }
 
     override fun resultado(Valor: Resultado) {
+
         if(referencia != null){
             if(Valor == Resultado.ERRO){
                 Alertas.criarAlerter(referencia!!, R.string.ErroEnviar, R.string.Atencao, 10000).show()
             } else {
+                val contexto = context
+                if(contexto != null){
+                    val Horas = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+                    val Dia = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+                    Dados.salvarLoja(contexto, LojaInfo?.id!!, "$Horas:$Dia")
+                    ProgressoFila?.isEnabled = false
+                }
                 val alerta = Alertas.criarAlerter(referencia!!, R.string.Atualizando, R.string.Aguardando)
                 alerta.show()
 
@@ -168,6 +211,7 @@ class FragmentResumo : Fragment(), FunctionsInterface {
                             NumeroAvaliacoes?.text = String.format(NovaLoja?.quantidadeAvaliacoesFila.toString() + " "+ getString(R.string.Avalicoes))
                             alerta.setDuration(1000)
                             LojaInfo = NovaLoja
+                            adaptador?.atualizarLoja(NovaLoja!!)
                             adaptador?.notifyDataSetChanged()
                             NovaLoja = null
                         }
